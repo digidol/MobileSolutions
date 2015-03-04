9ea421040932ab0096e0857e2bd873c5c0efe8aa
Neil Taylor <nst@aber.ac.uk>
2015-03-04 00:45:27 +0000

    Initial Version of Namer linked to the slides.

---
+++ ColourNamer/ColourNamer-Swift/ColourNamer-Swift/NamerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NamerViewController.swift
+//  ColourNamer-Swift
+//
+//  Created by Neil Taylor on 01/03/2015.
+//  Copyright (c) 2015 Aberystwyth University. All rights reserved.
+//
+
+import UIKit
+
+class NamerViewController: UIViewController {
+    
+    @IBOutlet var textField : UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+}
---
+++ ColourNamer/ColourNamer-Swift/ColourNamer-Swift/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  ColourNamer-Swift
+//
+//  Created by Neil Taylor on 01/03/2015.
+//  Copyright (c) 2015 Aberystwyth University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var currentColourLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    var name: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reset(self)
+        setCurrentName("Mid-Grey")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func getColor() -> UIColor {
+        
+        return UIColor(red: CGFloat(redSlider.value),
+                             green: CGFloat(greenSlider.value),
+                             blue: CGFloat(blueSlider.value),
+                             alpha: CGFloat(1.0))
+    }
+    
+    func updateBackgroundColor() {
+        self.view.backgroundColor = getColor()
+        currentColourLabel.text = "Current: Unknown"
+    }
+    
+    @IBAction func valueChanged(sender: AnyObject) {
+        updateBackgroundColor()
+    }
+    
+    @IBAction func reset(sender: AnyObject) {
+        redSlider.value = 0.5
+        greenSlider.value = 0.5
+        blueSlider.value = 0.5
+        updateBackgroundColor()
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue,
+                                  sender: AnyObject?) {
+        let namerScreen = segue.destinationViewController as? NamerViewController
+        namerScreen?.view.backgroundColor = self.view.backgroundColor
+    }
+
+    func setCurrentName(name: String) {
+        currentColourLabel.text = "Current: \(name)"
+    }
+    
+    @IBAction func setColourName(segue: UIStoryboardSegue) {
+        let source = segue.sourceViewController as? NamerViewController
+        
+        if var name = source?.textField?.text {
+                setCurrentName(name)
+        }
+        else {
+            setCurrentName("Unknown")
+        }
+    }
+
+    @IBAction func cancelSetColourName(segue: UIStoryboardSegue) {
+        // No action
+    }
+
+}
